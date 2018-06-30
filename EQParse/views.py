7cbed33d5fb0d635bb2f88c8d1ab2bebd2953a84
Czypser <Czypser@gmail.com>
2018-06-29 19:56:34 -0700

    Added a background image, started working on EQParse boxes for info and moved CSS out of the base.html file

--- EQParse/views.py
+++ EQParse/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
-
+from django.template import loader
 
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the EQParse index.")
-    return render(request, 'eqparseindex.html')
+    # loader.get_template('EQParse/eqparse.html')
+    # return HttpResponse('')
+    
+    return render(request, 'eqparse.html')
